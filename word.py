738f029a24032f77bb11f9d82b62cf7a90109669
Jurre de Vries <31882165+R4KE@users.noreply.github.com>
2018-09-20 09:44:54 +0200

    [ADD] some formatting and fixing choose-list mechanic

--- word.py
+++ word.py
@@ -21,21 +21,23 @@ def new_List():
     return newList
 
 def choose_List():
-    clear_Screen()
-    currentList = input("Choose list: ")
-    if os.path.isfile(currentList):
-        read_Words(currentList)
-        return(currentList);
-        manage_List()
-    else:
-        print("ewa nee bro")
+    while True:
+        clear_Screen()
+        currentList = input("Choose list: ")
+        if os.path.isfile(currentList):
+            read_Words(currentList)
+            return(currentList);
+            manage_List()
+            break
+        else:
+            print("NOOO")
 
 def manage_List():
     clear_Screen()
-    print("1 = choose list")
-    print("2 = make list")
-    print("3 = remove list")
-    print("4 = go back")
+    print("| {:40} |".format("1 = choose list"))
+    print("| {:40} |".format("2 = make list"))
+    print("| {:40} |".format("3 = remove list"))
+    print("| {:40} |".format("4 = go back"))
     choice = input("Choose an option: ")
     if choice == "1":
         choose_List()
@@ -50,9 +52,9 @@ def main():
     #show list
     while True:
         clear_Screen()
-        print("1 = start the test")
-        print("2 = manage lists")
-        print("3 = quit")
+        print("| {:40} |".format("1 = start the test"))
+        print("| {:40} |".format("2 = manage lists"))
+        print("| {:40} |".format("3 = quit"))
         choice = input("Choose an option: ")
         if choice == "1":
             start_Test()
