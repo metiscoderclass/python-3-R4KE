da4cbc721c009919980cf3ababf4d68dd8af5e96
Jurre de Vries <31882165+R4KE@users.noreply.github.com>
2018-10-03 09:11:03 +0200

    [ADD] Fixing?

--- word.py
+++ word.py
@@ -1,69 +1,63 @@
 import os
 import time
 
-screenWidth = 25;
-
 def clear_Screen():
     os.system('cls')
 
-def read_Words(wordList):
-    wordList = open(wordList)
+def read_list():
+    # vraag gebruiker om naan van lijst
+    wordList = {}
+    currentList = input("Choose list: ")
+    if not os.path.isfile(currentList):
+        return
+    wordFile = open(currentList)
+    for item in wordFile:
+        if not item == '':
+            wordNL, wordENG = item.strip('\n').split("=")
+            print(wordNL + " = " + wordENG)
+            #{"hallo" : "hello"}
+            wordList[wordNL] = wordENG
     return wordList
 
-def new_List():
-    clear_Screen()
-    newList = input("Name the new list: ")
-    read_Words(newList)
-    return newList
+def change_list():
+    wordList = read_list()
+    # toevoegen of verwijderen?
+    # bestand opslaan
 
-def choose_List():
-    while True:
-        clear_Screen()
-        currentList = input("Choose list: ")
-        if os.path.isfile(currentList):
-            wordList = read_Words(currentList)
-            return(currentList);
-            break
-            manage_List()
-        else:
-            print("NOOO")
-
+# functies = {"1": choose_list, }
+# functies[choice]()
 def manage_List():
     clear_Screen()
     print("| {:40} |".format("1 = choose list"))
-    print("| {:40} |".format("2 = make list"))
+    print("| {:40} |".format("2 = change list"))
     print("| {:40} |".format("3 = remove list"))
     print("| {:40} |".format("4 = go back"))
     choice = input("Choose an option: ")
     if choice == "1":
-        choose_List()
+        readList()
     elif choice == "2":
-        new_List()
+        change_List()
 
 def start_Test():
-    wordList = read_Words(currentList)
     for item in wordList:
         if not item == '':
-            word1, word2 = item.strip('\n').split("=")
-            print(word1 + " = " + word2)
-            input = input("Define: " + word1)
-            if input == word2:
+            wordNL, wordENG = item.strip('\n').split("=")
+            print(wordNL + " = " + wordENG)
+            input = input("Define: " + wordNL)
+            if input == wordENG:
                 print("dipp")
 
 def main():
     #show list
-    while True:
-        clear_Screen()
-        print("| {:40} |".format("1 = start the test"))
-        print("| {:40} |".format("2 = manage lists"))
-        print("| {:40} |".format("3 = quit"))
-        choice = input("Choose an option: ")
-        if choice == "1":
-            start_Test()
-            break
-        elif choice == "2":
-            manage_List()
-            break
+    clear_Screen()
+    print("| {:40} |".format("1 = start the test"))
+    print("| {:40} |".format("2 = manage lists"))
+    print("| {:40} |".format("3 = quit"))
+    choice = input("Choose an option: ")
+    if choice == "1":
+        start_Test()
+    elif choice == "2":
+        manage_List()
 
 
 main()
