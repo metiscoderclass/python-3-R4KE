22f6d6801ca3a59e7e9efc2319d5ff48a88cdcbb
Jurre de Vries <31882165+R4KE@users.noreply.github.com>
2018-10-09 15:38:39 +0200

    [ADDED] Errors

--- word.py
+++ word.py
@@ -1,22 +1,22 @@
 import os
 import time
 
+screenWidth = 10;
+
 def clear_Screen():
     os.system('cls')
 
 def read_list():
-    # vraag gebruiker om naan van lijst
+    # vraag gebruiker om naam van lijst
     clear_Screen()
-    wordList = {}
     currentList = input("Choose list: ")
     if not os.path.isfile(currentList):
         return
     wordFile = open(currentList)
+    wordList = {}
     for item in wordFile:
         if not item == '':
             wordNL, wordENG = item.strip('\n').split("=")
-            print(wordNL + " = " + wordENG)
-            #{"hallo" : "hello"}
             wordList[wordNL] = wordENG
     return wordList
 
@@ -42,31 +42,41 @@ def manage_List():
         manage_List()
 
 def start_Test():
-    for item in wordList:
-        if not item == '':
-            wordNL, wordENG = item.strip('\n').split("=")
-            print(wordNL + " = " + wordENG)
-            input = input("Define: " + wordNL)
-            if input == wordENG:
-                print("dipp")
-            else:
-                print("Ded")
+    correct = 0
+    incorrect = 0
+    while True:
+        for item in wordList:
+            if not item == '':
+                wordNL, wordENG = item.strip('\n').split("=")
+                print(wordNL + " = " + wordENG)
+                input = input("Define: " + wordNL)
+                if input == wordENG:
+                    print("NICEEEEEE")
+                else:
+                    print("Ded")
+                    return
+
+def menuLine(line):
+    newLine = (("| {:" + str(screenWidth - 3) + "}").format(line))
+    print(newLine)
+
 
 def main():
     #show list
-    clear_Screen()
-    print("| {:40} |".format("1 = start the test"))
-    print("| {:40} |".format("2 = manage lists"))
-    print("| {:40} |".format("3 = quit"))
     choice = input("Choose an option: ")
-    if choice == "1":
-        start_Test()
-    elif choice == "2":
-        manage_List()
-    elif choice == "3":
-        manage_List()
-    else:
-        main()
+    while choice != "4":
+        clear_Screen()
+        menuLine("1 = start the test")
+        print("| {:40} |".format("1 = start the test"))
+        print("| {:40} |".format("2 = manage lists"))
+        print("| {:40} |".format("3 = quit"))
+        choice = input("Choose an option: ")
+        if choice == "1":
+            start_Test()
+        elif choice == "2":
+            manage_List()
+        elif choice == "3":
+            manage_List()
 
 
 main()
