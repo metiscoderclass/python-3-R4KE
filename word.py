658384ffcf0e157dc2662dbdee5c17ead3b16be2
Jurre de Vries <31882165+R4KE@users.noreply.github.com>
2018-11-01 09:03:38 +0100

    [ADDED] so much cool shit

--- word.py
+++ word.py
@@ -2,7 +2,7 @@ import os
 import time
 import glob, os
 
-screenWidth = 10;
+screenWidth = 50;
 parent_dir = ""
 
 def clear_Screen():
@@ -11,54 +11,56 @@ def clear_Screen():
 def select_list():
     # vraag gebruiker om naam van lijst
     clear_Screen()
+    border_line()
     for txt_file in glob.glob(os.path.join(parent_dir, '*.txt')):
-        print (txt_file)
+        menu_Line("• " + txt_file)
+    border_line()
     currentList = input("Choose list: ")
     if not os.path.isfile(currentList):
         return
     wordFile = open(currentList)
-    wordlist = {}
-    #for item in wordFile:
-    #    if not item == '':
-    #        wordNL, wordENG = item.strip('\n').split("=")
-    #        wordList[wordNL] = wordENG
-    return wordlist
+    worddict = {}
+    return worddict
 
-def change_list():
-    print("test")
-    #while loop om te checken als er een file geselect is anders: break
-    # toevoegen of verwijderen?
-    # bestand opslaa n
+def add_words(worddict):
+    for item in worddict:
+        print(item)
+    newword = input("Add word: ")
+    worddict.write(newword)
+    worddict.close()
 
-def add_Remove():
-    print("test")
+    #add or remove lists
+
+def remove_words():
+    for item in worddict:
+        print(item)
     #add or remove lists
 
 # functies = {"1": choose_list, }
 # functies[choice]()
-def manage_List():
+def manage_List(worddict):
     clear_Screen()
-    #print(currentList)
-    menuLine("1 = select list")
-    menuLine("2 = change list")
-    menuLine("3 = remove/add list")
-    menuLine("4 = go back")
+    border_line()
+    menu_Line("1 = add list")
+    menu_Line("2 = remove list")
+    menu_Line("3 = add words")
+    menu_Line("4 = remove words")
+    menu_Line("5 = go back")
+    border_line()
     choice = input("Choose an option: ")
-    if choice == "1":
-        select_list()
-    elif choice == "2":
-        change_List()
-    elif choice == "3":
-        add_Remove()
+    if choice == "3":
+        add_words(worddict)
     elif choice == "4":
+        remove_words()
+    elif choice == "5":
         main()
 
-def start_Test(wordlist):
+def start_Test(worddict):
     #neem de lijst en overhoor
     correct = 0
     incorrect = 0
     while True:
-        for item in wordlist:
+        for item in worddict:
             if not item == '':
                 wordNL, wordENG = item.strip('\n').split("=")
                 input = input("Define: " + wordNL)
@@ -71,30 +73,33 @@ def start_Test(wordlist):
                     incorrect += 1
                     return
 
-def menuLine(line):
-    #print functie
-    newLine = (("| {:" + str(screenWidth - 3) + "}").format(line))
+def border_line():
+    borderWidth = screenWidth + 4
+    print("="*borderWidth)
+
+#print functie
+def menu_Line(line):
+    newLine = (("| {:" + str(screenWidth) + "} |").format(line))
     print(newLine)
 
 
 def main():
     #show list
     clear_Screen()
-    menuLine("1 = Start the test")
-    menuLine("2 = Select a list")
-    menuLine("3 = Manage lists")
-    menuLine("4 = quit")
+    border_line()
+    menu_Line("1 = Start the test")
+    menu_Line("2 = Manage lists")
+    menu_Line("3 = quit")
+    border_line()
     choice = input("Choose an option: ")
-    wordlist = {}
+    worddict = {}
     while choice != "4":
-        if choice in ["1", "3"]:
-            wordlist = select_list()
+        if choice in ["1", "2"]:
+            worddict = select_list()
         if choice == "1":
-            start_Test(wordlist)
+            start_Test(worddict)
         elif choice == "2":
-            select_list()
-        elif choice == "3":
-            manage_List()
+            manage_List(worddict)
         choice = input("Choose an option: ")
 
 main()
